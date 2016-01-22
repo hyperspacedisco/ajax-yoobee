b64c1f193fee06302a9d9ea0512c7175b38af919
Sarah Lyne <lynesarah@rocketmail.com>
2016-01-22 15:23:24 +1300

    added ajax to connect to yoobee.net

--- js/ajaxyoobee.js
+++ js/ajaxyoobee.js
@@ -1,1 +1,31 @@
-alert("hello");
+$(document).ready(function({
+
+	$('#note-form').submit( function(e){
+
+
+		e.preventdefault();
+
+		//grab note from form
+		$('#note').val();
+
+		if ( ($.trim('#note').val()) == 0 ){
+
+			return;
+		}
+		
+		$.ajax({
+			url: "http://sarah.lyne.yoobee.net.nz/ajax_yoobee/ajax-yoobee/api/ajaxyoobee.php",
+			data: { note: $('#note').val()},
+			success: function(serverData){
+
+
+			},
+			error: function(){
+				alert("there was a problem");
+			}
+			});
+		
+	});
+
+
+}));
