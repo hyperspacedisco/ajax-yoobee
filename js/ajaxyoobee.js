046656ce5923a58ceeb10a9dbf29160f02e11c9a
Sarah Lyne <lynesarah@rocketmail.com>
2016-01-26 13:06:20 +1300

    more jquery debugging

--- js/ajaxyoobee.js
+++ js/ajaxyoobee.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
 	$('#note-form').submit(function(e){
 
 
-		e.preventdefault();
+		e.preventDefault();
 
 		//grab note from form
 		$('#note').val();
 
-		if ( ($.trim('#note').val()) == 0 ){
+		if( $.trim($('#note').val()) == '' ){
 
 			return;
 		}
 		
 		$.ajax({
-			url: "http://sarah.lyne.yoobee.net.nz/ajax_yoobee/ajax-yoobee/api/ajaxyoobee.php",
-			data: { note: $('#note').val()},
+			url: "http://sarah.lyne.yoobee.net.nz/ajax_yoobee/ajaxyoobee.php",
+			data: { note: $('#note').val() },
 			success: function(serverData){
 				console.log(serverData);
 
